c2e4b5bfb8f47ae0d115c846783aef701c5d773c
Niels Swimberghe <nielsswimberghe@gmail.com>
2020-10-25 23:31:09 -0400

    Initial Commit

---
+++ TwilioBlazorPhonecalls.Server/Controllers/TokenController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Twilio.Jwt;
+using Twilio.Jwt.Client;
+
+namespace TwilioBlazorPhonecalls.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        public TokenController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        
+        [HttpGet]
+        [EnableCors("BlazorClientPolicy")]
+        public string Get()
+        {
+            string twilioAccountSid = configuration["TwilioAccountSid"];
+            string twilioAuthToken =  configuration["TwilioAuthToken"];
+            string twiMLApplicationSid =  configuration["TwiMLApplicationSid"];
+            string twilioPhoneNumber =  configuration["TwilioPhoneNumber"];
+
+            HashSet<IScope> scopes = new HashSet<IScope>
+            {
+                new IncomingClientScope("blazor_client"),
+                new OutgoingClientScope(twiMLApplicationSid)
+            };
+
+            var capability = new ClientCapability(twilioAccountSid, twilioAuthToken, scopes: scopes);
+
+            return capability.ToJwt();
+        }
+    }
+}
---
+++ TwilioBlazorPhonecalls.Server/Controllers/VoiceController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+using Twilio.TwiML.Voice;
+
+namespace TwilioBlazorPhonecalls.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VoiceController : TwilioController
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<VoiceController> logger;
+
+        public VoiceController(IConfiguration configuration, ILogger<VoiceController> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public TwiMLResult Post([FromForm] string to, [FromForm] string from)
+        {
+            string twilioPhoneNumber =  configuration["TwilioPhoneNumber"];
+            
+            var response = new VoiceResponse();
+            var dial = new Dial();
+            if (to == twilioPhoneNumber)
+            {
+                logger.LogInformation($"Calling blazor_client");
+                dial.CallerId = from;
+                dial.Client("blazor_client");
+            }
+            else
+            {
+                logger.LogInformation($"Calling {to}");
+                dial.CallerId = twilioPhoneNumber;
+                dial.Number(to);
+            }
+
+            response.Append(dial);
+
+            return TwiML(response);
+        }
+    }
+}
