653bfb1f19a9aa65b3f5eb43c5a0cb710fb500c2
Niels Swimberghe <nielsswimberghe@gmail.com>
2021-06-06 16:17:32 -0400

    update for demo

--- UpdateTwilioWebhooks.ps1
+++ UpdateTwilioWebhooks.ps1
@@ -1,5 +1,5 @@
-$ApplicationSid = "AP357100fa6dc6dc0fea75fbd6108e60cb";
-$TwilioPhonenumber = "+14324232980";
+$ApplicationSid = "AP24aaf7938ece2d44e2f89b13131c4e41";
+$TwilioPhonenumber = "+17032151589";
 
 # Make sure ngrok is running before running this
 $NgrokApi = 'http://127.0.0.1:4040/api';
@@ -11,3 +11,4 @@ twilio api:core:applications:update --sid=$ApplicationSid --voice-url=$PublicOut
 
 $PublicIncomingVoiceUrl = "$PublicBaseUrl/voice/incoming";
 twilio phone-numbers:update $TwilioPhonenumber --voice-url=$PublicIncomingVoiceUrl --voice-method=POST;
+
